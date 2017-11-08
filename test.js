02fa3437d6c1523eb5830f3999b73f4c5bc9440e
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-11-07 22:11:53 -0600

    edits

--- test.js
+++ test.js
@@ -13,9 +13,9 @@ describe('director app unit tests', function() {
     it('should verify server is running', function() {
         chai.request('http://localhost:8080')
         .get('/')
-        .end(function (err, res) {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
+        .then(function (err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
         });
     });
 });

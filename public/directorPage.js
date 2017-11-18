55543724e347f5ccd979cfff181e59dc70af4904
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-11-18 10:56:19 -0600

    fixed CORS issue

--- public/directorPage.js
+++ public/directorPage.js
@@ -1,10 +1,11 @@
+
 url = "http://localhost:9000/actor";
 $.getJSON(url, function(data) {
   console.log(data.notes);
     let html = "";
     for (var i = 0; i < data.notes.length; i++) {
-        console.log(data.notes[i].productionName);
-
+        // console.log(data.notes[i].productionName);
+        console.log(data.notes[i].actorName);
      html += 
             `<div class="dropdown">
             <button class="dropbtn">`+ data.notes[i].productionName + `</button>
--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.listen(PORT, function(err){
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', function(req, res) {
     res.send('index');

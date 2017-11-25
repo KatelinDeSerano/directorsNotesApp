9cf0658a15f57a67bf75bdca55bbe1e383ba35df
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-11-25 00:45:59 -0600

    added minimal styling

--- public/actorPage.js
+++ public/actorPage.js
@@ -11,32 +11,8 @@ let displayProductions = (data) => {
     $('#productionName').html(html);
 };
 
-// let displayNotes = (data) => {
-//     let html = "";
-//     for (var i = 0; i < data.notes.length; i++) {
-//       html += 
-//             `<h4>`+ data.notes[i].text + `<h4>`;
-//     }
-//     $('#notesDisplay').html(html);
-// };
-
-// function handleProductionSelect(productionName) {
-//     if ()
-//     $("notesDisplay").text();
-//   };
-
-// $("button").click(function() {
-//     $.ajax({
-//         url: "test.html",
-//         context: document.body,
-//         success: function(){
-//           $(this).addClass("done");
-//         }
-//     });  
-// });
-
 function displayNotes(productionId){
-   // console.log(productionId);
+   
     var request = $.ajax({
         url: baseUrl + "/note",
         method: "GET",
@@ -48,18 +24,16 @@ function displayNotes(productionId){
          for (i in notesArray) {
            console.log( notesArray[i])
             html += 
-                  `<h1>`+ notesArray[i] + `\n </h1>`;  
+                  `<div class="noteSnippet">
+                  <h3>"`+ notesArray[i] + `" \n </h3></div>`;  
         }
         $('#notesDisplay').html(html);
     });
-
-   
 };
 
 let displayError = (error) => {
     console.log("Error");
 };
-
 
 $.ajax ({
     url: baseUrl + "/productions",
@@ -67,13 +41,4 @@ $.ajax ({
     dataType: "json",
     success: displayProductions,
     error: displayError
-});
- 
-// $.ajax ({
-//     url: baseUrl + "/notes",
-//     type: "GET",
-//     dataType: "json",
-//     success: displayNotes,
-//     error: displayError
-// });
-
+}); 

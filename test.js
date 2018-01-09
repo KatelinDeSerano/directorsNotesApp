6531cd9de9af5cd25d22f807c1e6b8db49a8e426
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-09 11:46:33 -0600

    basic tests for server running

--- test.js
+++ test.js
@@ -8,30 +8,32 @@ const { app, closeServer, runServer } = require('./server.js');
 chai.use(chaiHttp);
 var expect = require('chai').expect;
 
-describe('Productions', function() {
 
-    before(function() {
-      return runServer();
+describe('director app unit tests', function () {
+    before(function () {
+        return runServer();
     });
-  
-    after(function() {
-      return closeServer();
+    after(function () {
+        return closeServer();
     });
-  
-    it('should list productions on GET', function() {
-        return chai.request('localhost/directors-notes-app');
-        const userId = "5a47c3204f476e1e7be3ce69"
-          .get('/productions/director/userId')
-          .then(function(res) {
-            expect(res).to.have.status(200);
-            expect(res).to.be.json;
-            expect(res.body).to.be.a('array');
-            expect(res.body.length).to.be.above(0);
-            res.body.forEach(function(item) {
-              expect(item).to.be.a('object');
-              expect(item).to.have.all.keys(
-                '_id', 'director', 'productionName', 'actors')
-            });
-          });
-      });
-})
+
+    //dummy test
+    it('should take less than 500ms', function (done) {
+        setTimeout(done, 300);
+    });
+
+    //dummy test
+    it('should take less than 500ms', function (done) {
+        setTimeout(done, 300);
+    });
+
+    //test server running
+    it('should verify server is running', function () {
+        return chai.request('http://localhost:8080/')
+            .get('/')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            })
+    });
+
+});

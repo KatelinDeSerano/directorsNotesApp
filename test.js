b99271eae348738fec55727f10dbd3e357ec70fc
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-08 21:56:22 -0600

    added test for Productions GET

--- test.js
+++ test.js
@@ -39,4 +39,32 @@ describe('director app unit tests', function () {
 });
 
 
+describe('Productions', function() {
 
+    before(function() {
+      return runServer();
+    });
+  
+    after(function() {
+      return closeServer();
+    });
+  
+    it('should list productions on GET', function() {
+     
+      return chai.request(app)
+        .get('/productions')
+        .then(function(res) {
+  
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+  
+          res.body.should.have.length.of.at.least(1);
+  
+          res.body.forEach(function(item) {
+            item.should.be.a('object');
+            item.should.include.keys('_id', 'productionName', 'director', 'actors');
+          });
+        });
+    });
+})

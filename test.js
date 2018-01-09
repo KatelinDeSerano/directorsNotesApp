5079a44e9bdc36f1aba6d4bd189abf2b10b83359
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-09 17:17:30 -0600

    GET test for productions

--- test.js
+++ test.js
@@ -37,3 +37,31 @@ describe('director app unit tests', function () {
     });
 
 });
+
+describe('Productions', function() {
+
+    before(function() {
+      return runServer();
+    });
+  
+    after(function() {
+      return closeServer();
+    });
+  
+    it('should list items on GET', function() {
+      return chai.request(app)
+      const userId = "5a47c3204f476e1e7be3ce69"
+        .get('/productions/actor/userId')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body.length).to.be.above(0);
+          res.body.forEach(function(item) {
+            expect(item).to.be.a('object');
+            expect(item).to.have.all.keys(
+              '_id', 'director', 'productionName', 'actors')
+          });
+        });
+    });
+})
